2e008437b6aca5ada826ad20e118584e8f9df552
James <jhandshoe@sbcglobal.net>
2017-10-25 21:17:44 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { AngularFireDatabase } from 'angularfire2/database'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private af: AngularFireDatabase) {
+      this.af.object('connected').subscribe(data => {
+        console.log(data);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDTKjf_mN1v07gaihx05YTUHfBqK-85ctg',
+  authDomain: 'psangularfire-d95e3.firebaseapp.com',
+  databaseURL: 'https://psangularfire-d95e3.firebaseio.com',
+  projectId: 'psangularfire-d95e3',
+  storageBucket: 'psangularfire-d95e3.appspot.com',
+  messagingSenderId: '221210446186'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
-  providers: [],
+  providers: [
+    AngularFireDatabase
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
